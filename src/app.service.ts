88b4b742a99f2ce75ce98982b6966eecc2a90891
g.ligetta <g.ligetta@notomia.com>
2023-02-13 17:47:02 +0100

    second commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h2>Ciao Mondooooooo!</h2>';
+    return '<h2>Bla bla!</h2>';
   }
 }
