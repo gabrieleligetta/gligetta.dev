bb2433a62f78c4b7e76c84f7ddd83de603ac572b
g.ligetta <g.ligetta@notomia.com>
2023-02-14 12:59:15 +0100

    second commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<div style="display: flex;justify-content: center;align-items: center;width: 100%"><img src="https://storage.cloud.google.com/workspace-per-bots-ligetta-daisy-bkt-us/Immagini/An%20oil%20pastel%20drawing%20of%20an%20annoyed%20cat%20in%20a%20spaceship.webp" alt="gatto"></div>';
+    return '<div style="display: flex;justify-content: center;align-items: center;width: 100%"><img src="/milord.jpg" alt="gatto"></div>';
+    // return '<div style="display: flex;justify-content: center;align-items: center;width: 100%"><img src="/cat.webp" alt="gatto"></div>';
   }
 }
