232fb3e47e1ba225759aab8c7336d72d0c121dda
g.ligetta <g.ligetta@notomia.com>
2023-02-14 14:25:33 +0100

    second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'img'),
+      rootPath: join(__dirname, '..', 'public'),
     }),
   ],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<div style="display: flex;justify-content: center;align-items: center;width: 100%"><img src="/milord.jpg" alt="gatto"></div>';
-    // return '<div style="display: flex;justify-content: center;align-items: center;width: 100%"><img src="/cat.webp" alt="gatto"></div>';
+    return '<div style="display: flex;justify-content: center;align-items: center;width: 100%"><img src="/milord.jpg" alt="milord"></div>';
+
   }
 }
