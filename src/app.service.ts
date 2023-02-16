7b44e74a8b2346524e0507edb0e08eea552eae71
g.ligetta <g.ligetta@notomia.com>
2023-02-16 10:32:00 +0100

    added 100% height to center img

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<div style="display: flex;justify-content: center;align-items: center;width: 100%"><img src="/milord.jpg" alt="milord"></div>';
+    return '<div style="display: flex;justify-content: center;align-items: center;width: 100%;height: 100%"><img src="/milord.jpg" alt="milord"></div>';
   }
 }
