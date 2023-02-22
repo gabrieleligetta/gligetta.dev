b241651c764375f263dcc2adce992d2e503324f9
g.ligetta <g.ligetta@notomia.com>
2023-02-22 13:43:32 +0100

    added cv

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import * as path from 'path';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  public async getIndex(@Res() res: Response) {
+    res.sendFile(path.join(__dirname, '..', 'public', 'CV_Ligetta_2023.html'));
   }
 }
